8a512a52954d3230756d5aaf9d9b751254b1fe61
cmetzner93 <cmetzner@vols.utk.edu>
2021-11-14 09:10:57 -0500

    update function train_model, and move prediction process to different script

--- train_nb_svc_models.py
+++ train_nb_svc_models.py
@@ -27,17 +27,15 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-# Performance Metrics
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+# use pickle to save trained model
+import pickle
+
 
 # Set global variable seed for reproducibility
 seed = 0
 
 
-def init_classifier(clf: str) -> Tuple[SklearnClassifier, Dict[str, int, float]]:
+def init_classifier(clf_v: str) -> Tuple[SklearnClassifier, Dict[str, int, float]]:
     """
     Function that returns the classifier and parameter grid for hyper-parameter tuning.
 
@@ -55,31 +53,30 @@ def init_classifier(clf: str) -> Tuple[SklearnClassifier, Dict[str, int, float]]
         Dictionary containing hyper-parameters that should be investigated during gridsearch cv
     """
 
-    if clf == 'nb':
-        classifier = GaussianNB(random_state=seed)
+    if clf_v == 'nb':
+        clf = GaussianNB(random_state=seed)
         param_grid = {'var_smoothing': np.logspace(0, -9, num=100)}
 
-    elif clf == 'svc':
-        classifier = SVC(random_state=seed)
+    elif clf_v == 'svc':
+        clf = SVC(random_state=seed)
         param_grid = [
           {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
           {'C': [1, 10, 100, 1000], 'gamma': [0.1, 0.01, 0.001, 0.0001], 'kernel': ['rbf']},
          ]
 
-    return classifier, param_grid
+    return clf, param_grid
 
 
-def ml_classification(
+def train_model(
         X_train: PandasDataFrame,
         X_test: PandasDataFrame,
         y_train: PandasSeries,
         y_test: PandasSeries,
-        classifier: SklearnClassifier,
-        param_grid: Dict[str, Union[int, float, str]],
+        clf_v: str,
         cv_m: int) -> List[Union[float, List[float]]]:
-    """This function performs hyperparameter tuning via the function GridSearchCV, takes the best estimator of the respective
-    classifier for model evaluation. The splits during cross-validation are stratified.
-
+    """
+    This function performs hyperparameter tuning via the function GridSearchCV, takes the best estimator of the
+    respective classifier for model evaluation. The splits during cross-validation are stratified.
 
     Parameters
     ----------
@@ -91,46 +88,30 @@ def ml_classification(
         Series that contains the respective ground-truth labels for the training data.
     y_test: PandasSeries
         Series that contains the repsective ground-truth labels for the testing data.
-    classifier: SklearnClassifier
-        Variable that holds the respective initialized classifier.
-    param_grid: dict
-        A python dictionary that contains all parameters with their values for hyperparameter tuning.
+    clf_v: str
+        Variable that indicates the type of initialized classifier ('nb', 'svc')
     cv_m: int
-        Variable that holds number of stratified splits (folds).
+        Variable that holds number of stratified splits (folds). Stratified splits have similar label distribution.
 
     Returns
     -------
-    result_classifier: List
-        A list containing all the performance metrics of the model.
+    SklearnClassifier
+        Best model after hyperparameter-tuning
     """
+    # initialize classifier
+    clf, param_grid = init_classifier(clf_v=clf_v)
+
+    # perform hyper-parameter tuning
     cv_m = StratifiedKFold(cv_m)
-    grid_clf = GridSearchCV(classifier, param_grid, cv=cv_m, scoring='f1', n_jobs=-1)
+    grid_clf = GridSearchCV(clf, param_grid, cv=cv_m, scoring='f1', n_jobs=-1)
     grid_clf.fit(X_train, y_train)
 
+    # get best model after hyper-parameter tuning
     clf_best = grid_clf.best_estimator_
+    # train best model
     clf_best.fit(X_train, y_train)
 
-    y_pred = clf_best.predict(X_test)
-    # y_pred_proba = clf_best.predict_proba(X_test)
+    with open(f'trained_{clf_v}_model.pkl', 'wb') as f:
+        pickle.dump(clf_best, f)
 
-    precision = precision_score(y_test, y_pred, average='binary', pos_label=1)
-    recall = recall_score(y_test, y_pred, average='binary', pos_label=1)
-    f1 = f1_score(y_test, y_pred, average='binary', pos_label=1)
-
-    print(f'\nModel Performance for {clf_best}:')
-    print(f'Precision: {precision}')
-    print(f'Recall: {recall}')
-    print(f'F1-Score: {f1}')
-
-    # Plot a confusion matrix of the results
-    print("\n--- Confusion matrix for test data ---")
-    conf_matrix = confusion_matrix(y_test, y_pred)
-    print(conf_matrix)
-    tp = conf_matrix[0][0]
-    fp = conf_matrix[0][1]
-    fn = conf_matrix[1][0]
-    tn = conf_matrix[1][1]
-    result = [clf_best, f1, precision, recall, tp, fp, fn, tn]
-
-    return result
-
+    return clf_best
